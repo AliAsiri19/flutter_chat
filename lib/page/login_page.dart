4111b40375e3b77ef45870c9b5fb1c32849bd304
AliAsiri19 <tutorial1381@gmail.com>
2020-03-16 11:58:54 +0300

    Add app bar

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -6,6 +6,9 @@ class LoginPage extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: AppBar(title: Text('Login'),
+      centerTitle: true,
+      ),
       body: Container(
         child: Center(
           child: Container(
